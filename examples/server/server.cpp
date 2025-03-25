56e82d024435e8672432ee72eecfe632c15a5d65
Xuan Son Nguyen <son@huggingface.co>
2025-03-25 19:16:19 +0100

    fix server

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -1959,7 +1959,7 @@ struct server_context {
 
         // the update_slots() logic will always submit a maximum of n_batch or n_parallel tokens
         // note that n_batch can be > n_ctx (e.g. for non-causal attention models such as BERT where the KV cache is not used)
-        batch.clear();
+        batch = llama_batch_ext_ptr(ctx);
         metrics.init();
     }
 
