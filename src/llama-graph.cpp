418a31fb60d9f0dcd548a1a767ab3b7aa8f44886
Xuan Son Nguyen <son@huggingface.co>
2025-04-29 00:01:22 +0200

    llama-graph : fix text position for mrope

--- src/llama-graph.cpp
+++ src/llama-graph.cpp
@@ -55,13 +55,15 @@ void llm_graph_input_pos::set_input(const llama_ubatch * ubatch) {
     if (ubatch->pos && pos) {
         const int64_t n_tokens = ubatch->n_tokens;
 
-        if (ubatch->token && n_pos_per_embd > 1) {
-            // in case we're using M-RoPE with text tokens, convert the 1D positions to 4D
-            // the other dimensions are all 0, they are unused for text tokens
+        if (ubatch->token && n_pos_per_embd == 4) {
+            // in case we're using M-RoPE with text tokens, convert the 1D positions to 3D
+            // the other dimensions are the same, except for 4th dim which will be all 0
             std::vector<llama_pos> pos_data(n_tokens*n_pos_per_embd, 0);
             // copy the first dimension
             for (int i = 0; i < n_tokens; ++i) {
-                pos_data[i] = ubatch->pos[i];
+                pos_data[               i] = ubatch->pos[i];
+                pos_data[    n_tokens + i] = ubatch->pos[i];
+                pos_data[2 * n_tokens + i] = ubatch->pos[i];
             }
             ggml_backend_tensor_set(pos, pos_data.data(), 0, pos_data.size()*ggml_element_size(pos));
         } else {
