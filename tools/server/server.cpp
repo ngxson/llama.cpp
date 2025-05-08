aaebc3367d3a93a339673c7683f87444d97e21cc
Xuan Son Nguyen <son@huggingface.co>
2025-05-08 12:52:02 +0200

    add "has_multimodal" to /props

--- tools/server/server.cpp
+++ tools/server/server.cpp
@@ -4009,6 +4009,7 @@ int main(int argc, char ** argv) {
             { "default_generation_settings", ctx_server.default_generation_settings_for_props },
             { "total_slots",                 ctx_server.params_base.n_parallel },
             { "model_path",                  ctx_server.params_base.model.path },
+            { "has_multimodal",              ctx_server.mctx != nullptr },
             { "chat_template",               common_chat_templates_source(ctx_server.chat_templates.get()) },
             { "bos_token",                   common_token_to_piece(ctx_server.ctx, llama_vocab_bos(ctx_server.vocab), /* special= */ true)},
             { "eos_token",                   common_token_to_piece(ctx_server.ctx, llama_vocab_eos(ctx_server.vocab), /* special= */ true)},
