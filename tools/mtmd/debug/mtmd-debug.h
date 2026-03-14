388b674d24729074c11a9c13fd8647e09d40e86c
Xuan Son Nguyen <son@huggingface.co>
2026-03-14 12:10:47 +0100

    add MTMD_API to fix build

--- tools/mtmd/debug/mtmd-debug.h
+++ tools/mtmd/debug/mtmd-debug.h
@@ -9,9 +9,9 @@
 // Do not raise issues related to this debugging API
 
 // encode take the pre-processed f32 values, print the intermidiate values via cb_eval callback
-void mtmd_debug_encode_image(mtmd_context * ctx, const std::vector<std::vector<float>> & image);
-void mtmd_debug_encode_audio(mtmd_context * ctx, const std::vector<float> & input); // will be broadcasted to fit n_mel
+MTMD_API void mtmd_debug_encode_image(mtmd_context * ctx, const std::vector<std::vector<float>> & image);
+MTMD_API void mtmd_debug_encode_audio(mtmd_context * ctx, const std::vector<float> & input); // will be broadcasted to fit n_mel
 
 // preprocess take the raw input values
-void mtmd_debug_preprocess_image(mtmd_context * ctx, const std::vector<uint8_t> & rgb_values, int nx, int ny);
-void mtmd_debug_preprocess_audio(mtmd_context * ctx, const std::vector<float> & pcm_samples);
+MTMD_API void mtmd_debug_preprocess_image(mtmd_context * ctx, const std::vector<uint8_t> & rgb_values, int nx, int ny);
+MTMD_API void mtmd_debug_preprocess_audio(mtmd_context * ctx, const std::vector<float> & pcm_samples);
