36fe0b038834d3570daa6fd9af7c34185a95e2ab
Julian Brunner <julian.brunner@gmail.com>
2026-04-28 09:04:43 +0200

    consistent punctuation

--- include/llama.h
+++ include/llama.h
@@ -1135,7 +1135,7 @@ extern "C" {
     /// Does not write null terminator to the buffer.
     /// @return Returns the number of chars/bytes on success, no more than length.
     /// @return Returns a negative number on failure - the number of chars/bytes that would have been returned.
-    /// @return Returns INT32_MIN if the token is not in the vocabulary
+    /// @return Returns INT32_MIN if the token is not in the vocabulary.
     /// @param lstrip User can skip up to 'lstrip' leading spaces before copying (useful when encoding/decoding multiple tokens with 'add_space_prefix')
     /// @param special If true, special tokens are rendered in the output.
     LLAMA_API int32_t llama_token_to_piece(
@@ -1150,7 +1150,7 @@ extern "C" {
     /// @param text The char pointer must be large enough to hold the resulting text.
     /// @return Returns the number of chars/bytes on success, no more than text_len_max.
     /// @return Returns a negative number on failure - the number of chars/bytes that would have been returned.
-    /// @return Returns INT32_MIN if any of the tokens is not in the vocabulary
+    /// @return Returns INT32_MIN if any of the tokens is not in the vocabulary.
     /// @param remove_special Allow to remove BOS and EOS tokens if model is configured to do so.
     /// @param unparse_special If true, special tokens are rendered in the output.
     LLAMA_API int32_t llama_detokenize(
