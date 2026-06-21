97b817b37744c0e526980454a8f28f6acc4c1d8a
Xuan Son Nguyen <son@huggingface.co>
2026-06-21 16:29:08 +0200

    rm unused

--- tools/server/server-models.h
+++ tools/server/server-models.h
@@ -179,10 +179,6 @@ public:
     void unload(const std::string & name);
     void unload_all();
 
-    // download a new model, progress is reported via SSE
-    // to stop the download, call unload()
-    void download(common_params_model && model, common_download_opts && opts);
-
     struct update_status_args {
         server_model_status status;
         int exit_code = 0; // only valid if status == UNLOADED
